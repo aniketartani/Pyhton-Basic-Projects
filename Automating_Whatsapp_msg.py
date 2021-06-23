da9392f880b9a1417871e8b0d1e979d93665e5c5
Hemang Jiwnani <80636235+Hjiwnain@users.noreply.github.com>
2021-06-23 22:28:54 +0530

    Whatsapp message sender

---
+++ Automating_Whatsapp_msg.py
@@ -0,0 +1,15 @@
+#Please login to whatsapp web in Edge before running the given code
+
+
+#Automatic Whatsapp message sender
+import pywhatkit as k
+k.sendwhatmsg("+91 1234567890","Put your message here ",HH,MM)
+"""
+Explanation:-
+Pywhatkit is a python based library to automate tasks
+In pywhatkit there is a function defined with name sendwhatmsg which take 4 argument as parameter which are :- 
+1.Phone number with country code
+2.Message which u want to check
+3.Time in 24Hr clock formate
+"""
+##Thank for veiwing contributed by:-Hemang Jiwnani
